2b37707d2be945056232b225d8b29140f6683ee5
JoseScoto <77130128+JoseScoto@users.noreply.github.com>
2024-03-05 00:51:35 -0600

    Second lab completed!

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please type your grade percentage: ");
+
+        string gradeFromUser = Console.ReadLine();
+        int grade = int.Parse(gradeFromUser);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed.");
+        }
+        else
+        {
+            Console.WriteLine("You failed. Don't give up and try again!");
+        }
     }
 }
